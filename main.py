de273162de9d260621ad134a5ef46310283b634c
Yuhi Sakaguchi <yuhi2m06@gmail.com>
2022-06-25 12:50:10 +0900

    :+1: sort of pie chart order

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import os, re, time, datetime, sys, csv, pandas
+import os
+import re
+import time
+import datetime
+import sys
+import csv
+import pandas
 from bs4 import BeautifulSoup
-from os.path import join,dirname
-from selenium.webdriver import Chrome,ChromeOptions
+from os.path import join, dirname
+from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -17,14 +23,13 @@ def login():
     options.add_argument("--disable-popup-blocking")
     options.add_argument("--headless")
 
-
     driver = Chrome(ChromeDriverManager().install(), options=options)
 
     LOGIN_URL = "https://trade.sbineomobile.co.jp/login"
     driver.get(LOGIN_URL)
     time.sleep(0.5)
 
-    input_name = driver.find_element(By.NAME,"username")
+    input_name = driver.find_element(By.NAME, "username")
     input_name.send_keys(os.environ['USER_ID'])
     input_pass = driver.find_element(By.NAME, "password")
     input_pass.send_keys(os.environ['PASSWORD'])
@@ -34,10 +39,9 @@ def login():
     return driver
 
 
-
 def main():
     driver = login()
-    
+
     PORTFOLIO_URL = "https://trade.sbineomobile.co.jp/account/portfolio"
     driver.get(PORTFOLIO_URL)
     time.sleep(2)
@@ -47,7 +51,8 @@ def main():
     tables = soup.select('#portfolio-layout > section')
     table_length = len(tables)
     while True:
-        driver.execute_script("document.getElementById('portfolio-foot').scrollIntoView({behavior: 'smooth',block: 'start'});")
+        driver.execute_script(
+            "document.getElementById('portfolio-foot').scrollIntoView({behavior: 'smooth',block: 'start'});")
         time.sleep(1)
         html = driver.page_source.encode('utf-8')
         soup = BeautifulSoup(html, "html.parser")
@@ -57,31 +62,33 @@ def main():
         table_length = len(tables)
 
     all_dict = {
-        'code':[],
-        'name':[],
-        'amount':[],
-        'valuation':[],
-        'valuation_rate':[],
-        'acquisition_unit_price':[],
-        'pl':[],
-        'present_value':[]
+        'code': [],
+        'name': [],
+        'amount': [],
+        'valuation': [],
+        'valuation_rate': [],
+        'acquisition_unit_price': [],
+        'pl': [],
+        'present_value': []
     }
 
-
-
     for x in range(4, table_length):
-        code = re.findall(r'p .*?"ticker">(\d+?)</p>',str(tables[x]))[0]
-        name = re.findall(r'<a>\n +?(\S.+?)\n',str(tables[x]))[0]
-        amount = re.findall(r'保有数量</th>\n<td><span>(\d+?)</span>',str(tables[x]))[0]
-        amount = round(float(amount.replace(',','')))
-        valuation = re.findall(r'評価額</div>\n<span>(\S+?)\n',str(tables[x]))[0]
-        valuation = round(float(valuation.replace(',','')))
-        acquisition_unit_price = re.findall(r'平均取得単価</th>\n<td><span>\n\s*(.+?)\n',str(tables[x]))[0]
-        acquisition_unit_price = round(float(acquisition_unit_price.replace(',','')))
-        present_value = re.findall(r'現在値/前日比</th>\n<td>\n<span>(.*?)</span>',str(tables[x]))[0]
-        present_value = round(float(present_value.replace(',','')))
+        code = re.findall(r'p .*?"ticker">(\d+?)</p>', str(tables[x]))[0]
+        name = re.findall(r'<a>\n +?(\S.+?)\n', str(tables[x]))[0]
+        amount = re.findall(
+            r'保有数量</th>\n<td><span>(\d+?)</span>', str(tables[x]))[0]
+        amount = round(float(amount.replace(',', '')))
+        valuation = re.findall(r'評価額</div>\n<span>(\S+?)\n', str(tables[x]))[0]
+        valuation = round(float(valuation.replace(',', '')))
+        acquisition_unit_price = re.findall(
+            r'平均取得単価</th>\n<td><span>\n\s*(.+?)\n', str(tables[x]))[0]
+        acquisition_unit_price = round(
+            float(acquisition_unit_price.replace(',', '')))
+        present_value = re.findall(
+            r'現在値/前日比</th>\n<td>\n<span>(.*?)</span>', str(tables[x]))[0]
+        present_value = round(float(present_value.replace(',', '')))
         pl = round((present_value - acquisition_unit_price) * amount)
-        valuation_rate = round(float(pl/valuation*100),2)
+        valuation_rate = round(float(pl/valuation*100), 2)
 
         all_dict['code'].append(code)
         all_dict['name'].append(name)
@@ -93,18 +100,18 @@ def main():
         all_dict['present_value'].append(present_value)
 
     df_neomobile = pandas.DataFrame({
-        'コード':all_dict['code'],
-        '銘柄名':all_dict['name'],
-        '保有数量(株)':all_dict['amount'],
-        '評価額(円)':all_dict['valuation'],
-        '評価損益率(%)':all_dict['valuation_rate'],
-        '平均取得単価(円)':all_dict['acquisition_unit_price'],
-        '損益(円)':all_dict['pl'],
-        '現在値(円)':all_dict['present_value']
-        })
+        'コード': all_dict['code'],
+        '銘柄名': all_dict['name'],
+        '保有数量(株)': all_dict['amount'],
+        '評価額(円)': all_dict['valuation'],
+        '評価損益率(%)': all_dict['valuation_rate'],
+        '平均取得単価(円)': all_dict['acquisition_unit_price'],
+        '損益(円)': all_dict['pl'],
+        '現在値(円)': all_dict['present_value']
+    })
 
     driver.quit()
-    
+
     make_graph(df_neomobile)
 
 
--- make_graph.py
+++ make_graph.py
@@ -3,26 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import japanize_matplotlib
 
+
 def make_graph(port_folio_df):
-  category_df = pd.read_csv('./categories.csv', index_col=1)
+    category_df = pd.read_csv('./categories.csv', index_col=1)
 
-  pf_array = port_folio_df.to_numpy()
-  dict = {}
-  sum = 0
+    pf_array = port_folio_df.to_numpy()
+    dict = {}
+    sum = 0
 
-  for arr in pf_array:
-    sum += arr[3]
-    code = int(arr[0])
-    category = category_df.loc[code, 'category']
-    if category in dict:
-      dict[category] += arr[3]
-    else:
-      dict[category] = arr[3]
-
-  keys = np.array(list(dict.keys()))
-  values = np.array(list(dict.values()))
-  plt.figure(dpi=200)
-  plt.title("総額%d円"%sum, fontsize=18)
-  plt.pie(values, labels=keys, autopct="%1.1f%%", textprops={'size': 'small'},)
-  plt.savefig('portfolio_chart.png')
-
+    for arr in pf_array:
+        sum += arr[3]
+        code = int(arr[0])
+        category = category_df.loc[code, 'category']
+        if category in dict:
+            dict[category] += arr[3]
+        else:
+            dict[category] = arr[3]
+    sorted_list = sorted(dict.items(), key=lambda x: x[1])
+    keys = [x[0] for x in sorted_list]
+    values = [x[1] for x in sorted_list]
+    plt.figure(dpi=200)
+    plt.title("総額%d円" % sum, fontsize=18)
+    plt.pie(values, labels=keys, autopct="%1.1f%%", textprops={
+            'size': 'small'}, counterclock=True, startangle=90)
+    plt.savefig('portfolio_chart.png')
